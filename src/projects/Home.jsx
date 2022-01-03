1087475950d5bdb2d7e09abe29c517f722b7edef
unknown <ahmedomar1007@gmail.com>
2022-01-03 11:45:14 +0200

    use destructing, loop links in home page

--- src/projects/Home.jsx
+++ src/projects/Home.jsx
@@ -1,54 +1,70 @@
 import { Link } from 'react-router-dom'
 const Home = () => {
+  const links = [
+    {
+      id: 1,
+      url: 'employees-list',
+      title: 'Employees list',
+      desc: ' Componenet, list, useState, map, Tailwindcss',
+    },
+    {
+      id: 1,
+      url: 'posts',
+      title: 'Posts',
+      desc: 'Fetch json placeholder Posts API, Tailwindcss',
+    },
+    {
+      id: 1,
+      url: 'reviews',
+      title: 'Reviews',
+      desc: 'Random Reviews, next-prev Reviews, React-icons, Tailwindcss',
+    },
+    {
+      id: 1,
+      url: 'menu',
+      title: 'Menu',
+      desc: 'List menu , Categories toggle, Tailwindcss',
+    },
+    {
+      id: 1,
+      url: 'tabs',
+      title: 'Tabs',
+      desc: 'Tabs, Tailwindcss',
+    },
+    {
+      id: 1,
+      url: 'lorem',
+      title: 'Lorem',
+      desc: 'Lorem ipsum Generate, Tailwindcss',
+    },
+    {
+      id: 1,
+      url: 'color',
+      title: 'Color',
+      desc: 'Color Generate, Tailwindcss',
+    },
+    {
+      id: 1,
+      url: 'grocery',
+      title: 'Grocery',
+      desc: 'localStorage',
+    },
+  ]
   return (
     <div className='container p-5'>
       <h2 className='text-center text-4xl font-bold mb-8 p-5'>
         React Essential Ideas Snippets
       </h2>
-      <Link to='/employees-list' target='_blank'>
-        <h3 className='text-2xl text-blue-500'>Employees list</h3>
-        <p className='text-lg italic text-gray-500 pb-5'>
-          Componenet, list, useState, map, Tailwindcss
-        </p>
-      </Link>
-      <Link to='/posts' target='_blank'>
-        <h3 className='text-2xl text-blue-500'>Posts</h3>
-        <p className='text-lg italic text-gray-500 pb-5'>
-          Fetch json placeholder Posts API, Tailwindcss
-        </p>
-      </Link>
-      <Link to='/reviews' target='_blank'>
-        <h3 className='text-2xl text-blue-500'>Reviews</h3>
-        <p className='text-lg italic text-gray-500  pb-5'>
-          Random Reviews, next-prev Reviews, React-icons, Tailwindcss
-        </p>
-      </Link>
-      <Link to='/menu' target='_blank'>
-        <h3 className='text-2xl text-blue-500'>Menu</h3>
-        <p className='text-lg italic text-gray-500 pb-5'>
-          List menu , Categories toggle, Tailwindcss
-        </p>
-      </Link>
-      <Link to='/tabs' target='_blank'>
-        <h3 className='text-2xl text-blue-500'>Tabs</h3>
-        <p className='text-lg italic text-gray-500 pb-5'>Tabs, Tailwindcss</p>
-      </Link>
-      <Link to='/lorem' target='_blank'>
-        <h3 className='text-2xl text-blue-500'>Lorem</h3>
-        <p className='text-lg italic text-gray-500 pb-5'>
-          Lorem ipsum Generate, Tailwindcss
-        </p>
-      </Link>
-      <Link to='/color' target='_blank'>
-        <h3 className='text-2xl text-blue-500'>Color</h3>
-        <p className='text-lg italic text-gray-500 pb-5'>
-          Color Generate, Tailwindcss
-        </p>
-      </Link>
-      <Link to='/grocery' target='_blank'>
-        <h3 className='text-2xl text-blue-500'>grocery</h3>
-        <p className='text-lg italic text-gray-500 pb-5'>localStorage</p>
-      </Link>
+
+      {links.map((link) => {
+        const { id, url, title, desc } = link
+        return (
+          <Link to={url} target='_blank' key={id}>
+            <h3 className='text-2xl text-blue-500'>{title}</h3>
+            <p className='text-lg italic text-gray-500 pb-5'>{desc}</p>
+          </Link>
+        )
+      })}
     </div>
   )
 }
