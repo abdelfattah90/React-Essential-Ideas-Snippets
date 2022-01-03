4cfe0ba5165451853ec9a0763b74249c4bf7cb6e
unknown <ahmedomar1007@gmail.com>
2022-01-03 15:59:52 +0200

    try fix error

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import EmployeesList from './projects/01-employees-list/EmployeesList'
 import PostApp from './projects/02-posts/PostApp'
 import Reviews from './projects/03-reviews/Reviews'
 import Menu from './projects/04-menu/Index'
-import Tabs from './projects/05-tabs/Tabs'
-import Lorem from './projects/06-lorem-ipsum/Lorem'
-import Color from './projects/07-color-generator/Color'
+
 import NotFound from './projects/NotFound'
 
 function App() {
@@ -18,9 +16,7 @@ function App() {
         <Route path='/posts' element={<PostApp />} />
         <Route path='/reviews' element={<Reviews />} />
         <Route path='/menu' element={<Menu />} />
-        <Route path='/tabs' element={<Tabs />} />
-        <Route path='/lorem' element={<Lorem />} />
-        <Route path='/color' element={<Color />} />
+   
         <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
